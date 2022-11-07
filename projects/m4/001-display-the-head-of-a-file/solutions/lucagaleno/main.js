ea929b8fcc30ba7f086f44fbb628e5f8deb4d214
LG <luca.galeno@gmail.com>
2022-11-07 21:58:34 +0100

    feat(m4): extra solution of exe

--- projects/m4/001-display-the-head-of-a-file/solutions/lucagaleno/main.js
+++ projects/m4/001-display-the-head-of-a-file/solutions/lucagaleno/main.js
@@ -3,26 +3,57 @@
 const fileName = 'testFile.txt'
 const numberOfHeadLines = 10;
 
-const fs = require('fs/promises');
+// solution of the exe with async / await
+const fs1 = require('fs/promises');
 
 async function displayHeadFile() {
 
     try {
-        const data = await fs.readFile(fileName, { encoding: 'utf8' });
+        const data = await fs1.readFile(fileName, { encoding: 'utf8' });
         const line = data.split('\n');
         const linesNumber = line.length;
 
-        if (linesNumber < 10) throw `Error description: the head of the file contains less then ${numberOfHeadLines} lines.`;
+        if (linesNumber < numberOfHeadLines) throw `Error description: the head of the file contains less then ${numberOfHeadLines} lines.`;
 
         console.log('reading in progress...');
         for(let i = 0; i < numberOfHeadLines; i++){
             console.log(line[i]);
         }
-        console.log('reading finished.');
+        console.log('reading finished.\n');
 
     } catch (err) {
         console.log(`An error has occurred. The desired file does not exist or check the description error. ${err}`);
     }
 }
-
 displayHeadFile();
+
+// ____________________________________________________________
+
+//soultion of the exe without async - await
+
+const fs = require('fs');
+
+const myPromise = new Promise((resolve, reject) => {
+
+    const data = fs.readFileSync(fileName, {encoding: 'utf-8'})
+    const line = data.split('\n');
+    const linesNumber = line.length; 
+   
+    if (linesNumber >= numberOfHeadLines) {
+        
+        console.log(`reading in progress...`)
+        for(let i = 0; i < numberOfHeadLines; i++){
+            console.log(line[i]);
+        }
+        resolve()
+    } else {
+        reject()
+    }
+});
+
+myPromise
+    .then(() => console.log(`reading finished.`))
+    .catch(() => console.error('something went wrong'))
+    
+
+    
