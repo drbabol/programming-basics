0b8d27034132705ff3a54597ab73bf72c9813222
LG <luca.galeno@gmail.com>
2022-11-16 11:55:18 +0100

    feat(m4): refactoring exe 001 & 002

--- projects/m4/001-display-the-head-of-a-file/solutions/lucagaleno/main.js
+++ projects/m4/001-display-the-head-of-a-file/solutions/lucagaleno/main.js
@@ -1,7 +1,52 @@
+//soultion of the exe without async - await
 // 001-display-the-head-of-a-file
+// 002-display-the-tail-of-a-file
 
 const fileName = 'testFile.txt'
-const numberOfHeadLines = -10;
+const numberOfLines = -10;
+
+//function to check the file
+const fs = require('fs');
+
+const fileRead = (fileName) => {
+    
+    try {
+        const data = fs.readFileSync(fileName, {encoding: 'utf-8'});
+        const lines = data.split('\n');
+        return extractHeadTail(lines, numberOfLines)
+    } catch {
+        return console.error('File does not exist')
+    }
+}
+
+// function to extract the rows
+const extractHeadTail = (lines, numberOfLines) => {
+
+    return new Promise((resolve, reject) => {
+
+        const linesNumber = lines.length; 
+        const head = lines.slice(0, numberOfLines).toString().replaceAll(',','\n')
+        const tail = lines.slice(numberOfLines).toString().replaceAll(',','\n')
+
+        if (linesNumber >= Math.abs(numberOfLines)) {
+            console.log(`Reading in progress...`)
+            if (numberOfLines > 0){
+                console.log((head));
+            }else { 
+                console.log((tail));
+            }
+            resolve()
+
+        } else {
+            reject(console.error('There are not enough lines'))
+        }
+    })
+        .then(() => console.log(`Reading finished.`))
+        .catch(() => console.error('Error!'))
+}
+
+fileRead(fileName) 
+
 /*
 // solution of the exe with async / await
 const fs1 = require('fs/promises');
@@ -28,49 +73,3 @@ async function displayHeadFile() {
 displayHeadFile();
 */
 // ____________________________________________________________
-
-//soultion of the exe without async - await
-// 001-display-the-head-of-a-file
-// 002-display-the-tail-of-a-file
-
-//function to check the file
-const fs = require('fs');
-
-let fileRead = (fileName) => {
-
-    try {
-        const data = fs.readFileSync(fileName, {encoding: 'utf-8'})
-        const line = data.split('\n');
-        return line
-    } catch {
-        return console.error('file does not exist')
-    }
-}
-
-// function to extract the rows
-let extractHeadTail = (lines, numberOfHeadLines) => {
-
-    return new Promise((resolve, reject) => {
-
-        const linesNumber = lines.length; 
-        const head = lines.slice(0, numberOfHeadLines).toString().replaceAll(',','\n')
-        const tail = lines.slice(numberOfHeadLines).toString().replaceAll(',','\n')
-
-        if (linesNumber >= Math.abs(numberOfHeadLines)) {
-            console.log(`reading in progress...`)
-            if (numberOfHeadLines > 0){
-                console.log((head));
-            }else { 
-                console.log((tail));
-            }
-            resolve()
-
-        } else {
-            reject(console.error('there are not enough lines'))
-        }
-    })
-        .then(() => console.log(`reading finished.`))
-        .catch(() => console.error('Error!'))
-}
-
-extractHeadTail(fileRead(fileName, numberOfHeadLines),numberOfHeadLines)
